9e6d62a7cd0e8c8f3844d3ed14fbc8f9c2f4808c
Zubkov Dmitrii <dimazubkov2003@gmail.com>
2024-12-24 00:54:12 +0300

    Make it work

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,493 +1,225 @@
 #include "task2.h"
 
-#include <algorithm>
-#include <chrono>
-#include <cmath>
-#include <complex>
-#include <cstdio>
-#include <cstdlib>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <random>
-#include <string>
-
 #include "mpi.h"
 
-template <typename T>
-class TMatrix {
-   public:
-    TMatrix(const int n, const int m, const bool randomFill = true)
-        : N(n), M(m), Data_(new T[N * M]) {
-        if (randomFill) {
-            RandomFill();
-        }
-    }
-
-    TMatrix(TMatrix&& matrix) {
-        Data_ = matrix.Data_;
-        N = matrix.N;
-        M = matrix.M;
-    }
-
-    TMatrix<T>& operator=(TMatrix<T>&& matrix) {
-        if (this == &matrix) {
-            return *this;
-        }
-
-        delete[] Data_;
-        Data_ = std::exchange(matrix.Data_, nullptr);
-        N = std::exchange(matrix.N, 0);
-        M = std::exchange(matrix.M, 0);
-        return *this;
-    }
-
-    void RandomFill(int alpha = 1) {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<double> dis(-10.0, 10.0);
-
-        int i = 0;
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                Data_[N * c + r] = (i + 1) + (alpha * i);
-                i++;
-            }
-        }
-    }
-
-    bool FillFromFile(const std::string& filename) {
-        std::ifstream file(filename);
-
-        if (!file.is_open()) {
-            std::cerr << "Error opening file: " << filename << std::endl;
-            return false;
-        }
-
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                file >> Data_[N * c + r];
-            }
-        }
-
-        file.close();
-        return true;
-    }
-
-    bool FillFromBinFile(const std::string& filename) {
-        std::ifstream file(filename, std::ios::binary);
-        if (!file.is_open()) {
-            std::cerr << "Error opening file: " << filename << std::endl;
-            return false;
-        }
-
-        // в файле хранится по строкам, записываем по столбцам
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                file.read(reinterpret_cast<char*>(Data_ + N * c + r),
-                          sizeof(T));
-            }
-        }
-        return true;
-    }
-
-    T* operator[](int pos) const {
-        if (pos >= N) {
-            throw "Out of bound";
-        }
-        return &Data_[pos * M];
-    }
-
-    T* Data() { return Data_; }
-
-    int GetRows() const { return N; }
-
-    int GetCols() const { return M; }
-
-    ~TMatrix() { delete[] Data_; }
-
-   private:
-    int N, M;
-    T* Data_;
-};
-
-const double hbar = 1.0;
-
-class TScopeLogger {
-   public:
-    TScopeLogger(const char* scopeName, int size)
-        : StartTime(std::chrono::high_resolution_clock::now()) {
-        for (auto i = 0; i < size; i++) {
-            ScopeName.push_back(scopeName[i]);
-        }
-    }
-
-    ~TScopeLogger() {
-        auto endTime = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-            endTime - StartTime);
-        std::string scopeName(ScopeName.data(), ScopeName.size());
-        std::cout << "Exiting scope: " << scopeName
-                  << " (duration: " << duration.count() << " microseconds)"
-                  << std::endl;
-    }
-
-   private:
-    std::chrono::high_resolution_clock::time_point StartTime;
-    std::vector<char> ScopeName;
-};
-
-void FillFromVector(const TMatrix<std::complex<double>>& vector,
-                    TMatrix<std::complex<double>>& result) {
-    for (int i = 0; i < result.GetCols(); i++) {
-        for (int j = 0; j < result.GetCols(); j++) {
-            result[i][j] = vector[i][0] * std::conj(vector[j][0]);
-        }
-    }
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& stream, TMatrix<T>& matrix) {
-    int rows = matrix.GetRows();
-    int cols = matrix.GetCols();
-    int width = 10;
-
-    for (int r = 0; r < rows; ++r) {
-        for (int c = 0; c < cols; ++c) {
-            stream << std::setw(25) << *(matrix.Data() + rows * c + r) << " ";
-        }
-        stream << std::endl;
-    }
-
-    std::flush(stream);
-
-    return stream;
-}
-
-template <typename T>
-TMatrix<T> ScatterMatrix(bool mpiroot, int context, int N, int M, int NBlocks,
-                         int MBlocks, int& NBlock, int& MBlock, int& nrows,
-                         int& ncols, TMatrix<T>& globalA) {
-    int zero = 0;
-
-    int myrow, mycol;
-    int nprows, npcols;
-    Cblacs_gridinfo(context, &nprows, &npcols, &myrow, &mycol);
-
-    NBlock = N / nprows;
-    MBlock = M / npcols;
-
-    nrows = numroc_(&N, &NBlock, &myrow, &zero, &nprows);
-    ncols = numroc_(&M, &MBlock, &mycol, &zero, &npcols);
-
-    int sendr = 0, sendc = 0;
-    int recvr = 0, recvc = 0;
-
-    TMatrix<T> localA(nrows, ncols);
-
-    for (int r = 0; r < N; r += NBlocks, sendr = (sendr + 1) % nprows) {
-        sendc = 0;
-
-        int nr = NBlocks;
-        if (N - r < NBlocks) {
-            nr = N - r;
-        }
-
-        for (int c = 0; c < M; c += MBlocks, sendc = (sendc + 1) % npcols) {
-            int nc = MBlocks;
-            if (M - c < MBlocks) {
-                nc = M - c;
-            }
-
-            if (mpiroot) {
-                Czgesd2d(context, nr, nc, globalA.Data() + N * c + r, N, sendr,
-                         sendc);
-            }
-
-            if (myrow == sendr && mycol == sendc) {
-                Czgerv2d(context, nr, nc, localA.Data() + nrows * recvc + recvr,
-                         nrows, 0, 0);
-                recvc = (recvc + nc) % ncols;
-            }
-        }
-
-        if (myrow == sendr) {
-            recvr = (recvr + nr) % nrows;
-        }
-    }
-
-    return localA;
-}
-
-template <typename T>
-TMatrix<T> GatherMatrix(bool mpiroot, int context, int N, int M, int NBlocks,
-                        int MBlocks, TMatrix<T>& localA) {
-    int zero = 0;
-
-    int myrow, mycol;
-    int nprows, npcols;
-    Cblacs_gridinfo(context, &nprows, &npcols, &myrow, &mycol);
-
-    int NBlock = N / nprows;
-    int MBlock = M / npcols;
-
-    int nrows = numroc_(&N, &NBlock, &myrow, &zero, &nprows);
-    int ncols = numroc_(&M, &MBlock, &mycol, &zero, &npcols);
-
-    int sendr = 0, sendc = 0;
-    int recvr = 0, recvc = 0;
-
-    TMatrix<T> result(N, M);
-    for (int r = 0; r < N; r += NBlocks, sendr = (sendr + 1) % nprows) {
-        sendc = 0;
-        int nr = NBlocks;
-        if (N - r < NBlocks) {
-            nr = N - r;
-        }
-
-        for (int c = 0; c < M; c += MBlocks, sendc = (sendc + 1) % npcols) {
-            int nc = MBlocks;
-            if (M - c < MBlocks) {
-                nc = M - c;
-            }
-
-            if (myrow == sendr && mycol == sendc) {
-                Czgesd2d(context, nr, nc, localA.Data() + nrows * recvc + recvr,
-                         nrows, 0, 0);
-                recvc = (recvc + nc) % ncols;
-            }
-
-            if (mpiroot) {
-                Czgerv2d(context, nr, nc, result.Data() + N * c + r, N, sendr,
-                         sendc);
-            }
-        }
-
-        if (myrow == sendr) {
-            recvr = (recvr + nr) % nrows;
-        }
-    }
-
-    return result;
-}
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 void fillVectorFromFile(std::vector<double>& w, const std::string& filename) {
-    std::ifstream inputFile(filename);
-
-    if (!inputFile) {
+    std::ifstream ifs(filename);
+    if (!ifs) {
         std::cerr << "Ошибка открытия файла: " << filename << std::endl;
         return;
     }
-
-    double number;
-    size_t index = 0;
-
-    while (index < w.size() && inputFile >> number) {
-        w[index] = number;
-        index++;
+    for (auto& val : w) {
+        if (!(ifs >> val)) break;
     }
-
-    inputFile.close();
 }
 
 unsigned long long factorial(int n) {
-    unsigned long long result = 1;
+    unsigned long long res = 1;
     for (int i = 2; i <= n; ++i) {
-        result *= i;
+        res *= i;
     }
-    return result;
+    return res;
 }
 
 unsigned long long binomialCoefficient(int n, int k) {
-    if (k > n) {
-        return 0;
-    }
-    if (k == 0 || k == n) {
-        return 1;
-    }
+    if (k > n) return 0;
+    if (k == 0 || k == n) return 1;
     return factorial(n) / (factorial(k) * factorial(n - k));
 }
 
-// double* gen_H_i(context& ctxt, int n_i, int i){
-//     double* H_i_loc;
-//     int* vecs;
+void CalcLocalRange(int rank, int nprocs, int N, int& rowsCount, int& offset) {
+    int base = N / nprocs, extra = N % nprocs;
+    rowsCount = base + (rank < extra);
+    offset = (rank < extra)
+             ? rank * (base + 1)
+             : extra * (base + 1) + (rank - extra) * base;
+}
 
-//     Partition part = compute_partition(ctxt.mpirank, ctxt.nprocs, n_i);
+double* GetLocalH(int rank, int nprocs, int ctx,
+                const std::vector<double>& a,
+                const std::vector<double>& w,
+                int N, int Emax, int i,
+                const std::vector<int>& sizes)
+{
+    int ones = Emax - i;
+    int N2 = 1 << N;
+    int localSize = sizes[i];
 
-//     H_i_loc = new double[n_i * part.local_rows];
-//     int n_ = 0;
-//     vecs = new int[n_i];
-//     for (int j = 0; j < N2; j++) {
-//         if (__builtin_popcount(j) == ones) {
-//             vecs[n_++] = j;
-//             // cout << bitset<32>(j).to_string().substr(32-p.N, p.N) << " ";
-//         }
-//     }
+    std::vector<int> allStates(localSize);
+    for (int j = 0, idx = 0; j < N2; ++j) {
+        if (__builtin_popcount(j) == ones) {
+            allStates[idx++] = j;
+        }
+    }
 
-//     for (int j = part.offset; j < part.offset + part.local_rows; j++) {
-//         for (int k = 0; k < n_i; k++) {
-//             if (j == k) {
-//                 double x = 0;
-//                 int index = 0;
-//                 while (vecs[j] > 0) {
-//                     if (vecs[j] & 1) {
-//                         x += p.w[p.N - index-1];
-//                     }
-//                     vecs[j] >>= 1;
-//                     index++;
-//                 }
-//                 H_i_loc[(j-part.offset)*n_i + k] = x;
-//             } else {
-//                 if (__builtin_popcount(vecs[j]^vecs[k]) != 2)
-//                     H_i_loc[(j-part.offset)*n_i + k] = 0;
+    int rowsCount, offset;
+    CalcLocalRange(rank, nprocs, localSize, rowsCount, offset);
 
-//                 int number = vecs[j]^vecs[k];
-//                 int index = 0;
-//                 while (number > 0) {
-//                     if (number & 1) {
-//                         number >>= 1;
-//                         H_i_loc[(j-part.offset)*n_i + k] = (number & 1) ?
-//                         p.a[p.N-index-2] : 0;
-//                     }
-//                     number >>= 1;
-//                     index++;
-//                 }
-//                 H_i_loc[(j-part.offset)*n_i + k] = 0;
-//             }
-//         }
-//     }
+    double* H_i_loc = new double[localSize * rowsCount]();
 
-//     double* H_i = NULL;
-//     if (ctxt.is_root) {
-//         H_i = new double[n_i*n_i];
-//         for (int j = 0; j < n_i; j++)
-//             for (int k = 0; k< n_i; k++)
-//                 H_i[j*n_i+k] = 0;
-//     }
-//     for (int proc = 0; proc < ctxt.nprocs; proc++){
-//         Cblacs_barrier(ctxt.ctxt, "All");
-//         if (ctxt.mpirank == proc) {
-//             Cdgesd2d(ctxt.ctxt, part.local_rows, n_i, H_i_loc,
-//             part.local_rows, 0, 0);
-//         }
-//         if (ctxt.is_root) {
-//             Partition loc_part = compute_partition(proc, ctxt.nprocs, n_i);
-//             Cdgerv2d(ctxt.ctxt, loc_part.local_rows, n_i,
-//             H_i+n_i*loc_part.offset, loc_part.local_rows, 0, proc);
-//         }
-//     }
+    for (int j = offset; j < offset + rowsCount; j++) {
+        for (int k = 0; k < localSize; k++) {
+            if (j == k) {
+                unsigned tmp = allStates[j];
+                double val = 0;
+                int bit_idx = 0;
+                while (tmp) {
+                    if (tmp & 1u) {
+                        val += w[N - bit_idx - 1];
+                    }
+                    tmp >>= 1;
+                    bit_idx++;
+                }
+                H_i_loc[(j - offset) * localSize + k] = val;
+            } else {
+                int x = allStates[j] ^ allStates[k];
+                if (__builtin_popcount(x) != 2) {
+                    continue;
+                }
+                double val = 0;
+                int bit_idx = 0;
+                while (x) {
+                    if (x & 1) {
+                        x >>= 1;
+                        val = (x & 1) ? a[N - bit_idx - 2] : 0;
+                    } else {
+                        x >>= 1;
+                    }
+                    bit_idx++;
+                }
+                H_i_loc[(j - offset) * localSize + k] = val;
+            }
+        }
+    }
 
-//     delete[] H_i_loc;
-//     delete[] vecs;
-//     return H_i;
-// }
+    double* localH = rank == 0 ? new double[localSize * localSize] : nullptr;
+    for (int p = 0; p < nprocs; ++p) {
+        Cblacs_barrier(ctx, "All");
+        if (rank == p) {
+            Cdgesd2d(ctx, rowsCount, localSize, H_i_loc, rowsCount, 0, 0);
+        }
+        if (rank == 0) {
+            int lrows, offs;
+            CalcLocalRange(p, nprocs, localSize, lrows, offs);
+            Cdgerv2d(ctx, lrows, localSize, localH + offs * localSize, lrows, 0, p);
+        }
+    }
 
-// void write_H_i_in_H(context& ctxt, Parameters& p, double* H, double* H_i, int
-// i){
-//     if (!ctxt.is_root)
-//         return;
+    delete[] H_i_loc;
+    return localH;
+}
 
-//     int start = 0;
-//     int size_loc = get_size_i(p, i);
-//     for (int j = 0; j < i; j++) {
-//         start += get_size_i(p, j);
-//     }
-//     int size = start;
-//     for (int j = i; p.Emax - j >= p.Emin; j++) {
-//         size += get_size_i(p, j);
-//     }
+void Gather(bool is_root, int Emax, int Emin, int N,
+                    double* H, const double* localH, int i, const std::vector<int>& sizes)
+{
+    if (!is_root) return;
 
-//     for (int j = 0; j < size_loc; j++) {
-//         for (int k = 0; k < size_loc; k++) {
-//             H[(j+start)*size + start + k] = H_i[j*size_loc + k];
-//         }
-//     }
-// }
+    int start = 0;
+    for (int j = 0; j < i; j++) {
+        start += sizes[j];
+    }
+
+    int size_loc = sizes[i];
+    int full_size = start;
+    for (int j = i; Emax - j >= Emin; ++j) {
+        full_size += sizes[j];
+    }
+
+    for (int row = 0; row < size_loc; ++row) {
+        std::copy(localH + row * size_loc,
+                  localH + (row + 1) * size_loc,
+                  H + (start + row) * full_size + start);
+    }
+}
+
+double* GetGlobalH(int rank, int nproc, int ctx,
+                         std::vector<double>& a, std::vector<double>& w,
+                         int N, int Emax, int Emin, std::vector<int>& sizes)
+{
+    int n = 0;
+    for (int i = 0; Emax - i >= Emin; i++) {
+        n += sizes[i];
+    }
+
+    double* H = (rank == 0 ? new double[n * n] : nullptr);
+
+    for (int i = 0; Emax - i >= Emin; ++i) {
+        double* localH = GetLocalH(rank, nproc, ctx, a, w, N, Emax, i, sizes);
+        Gather(rank == 0, Emax, Emin, N, H, localH, i, sizes);
+        delete[] localH;
+    }
+    return H;
+}
 
 int main(int argc, char** argv) {
-    int info = 0;
-
-    int N, k, Emin, Emax;
-    int NBlocks = 2, MBlocks = 2;
-
     MPI_Init(&argc, &argv);
-    int nprocs = 0, myrank = 0;
+    int nprocs = 0, rank = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-
-    bool mpiroot = (myrank == 0);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    bool mpiroot = (rank == 0);
 
     if (argc < 7) {
         if (mpiroot) {
-            std::cerr
-                << "Usage: ./mpirun -np <nprocs> N a_file w_file k Emin Emax"
-                << std::endl;
+            std::cerr << "Usage: ./mpirun -np <nprocs> N a_file w_file k Emin Emax\n";
         }
-
         MPI_Finalize();
         return 1;
     }
 
     std::stringstream stream;
+    int N, k, Emin, Emax;
     stream << argv[1] << " " << argv[4] << " " << argv[5] << " " << argv[6];
     stream >> N >> k >> Emin >> Emax;
 
     std::string fnameA(argv[2]);
     std::string fnameW(argv[3]);
 
-    std::vector<double> a(N - 1);
-    std::vector<double> w(N);
-
+    std::vector<double> a(N - 1), w(N);
     fillVectorFromFile(a, fnameA);
     fillVectorFromFile(w, fnameW);
 
     if (mpiroot) {
         std::cout << "A: ";
-        for (int i = 0; i < N - 1; i++) {
-            std::cout << a[i] << ' ';
-        }
+        for (auto val : a) std::cout << val << ' ';
         std::cout << "\nW: ";
-        for (int i = 0; i < N; i++) {
-            std::cout << w[i] << ' ';
-        }
+        for (auto val : w) std::cout << val << ' ';
         std::cout << "\n";
     }
 
     int n = 0;
-    std::vector<int> n_i(k + 1);
+    std::vector<int> sizes(k + 1);
     for (int i = 0; Emin <= Emax - i; i++) {
-        n_i[i] = binomialCoefficient(N, Emax - i);
-        n += n_i[i];
+        sizes[i] = binomialCoefficient(N, Emax - i);
+        n += sizes[i];
     }
-    TMatrix<std::complex<double>> globalH(n, n, false);
 
     int dims[2] = {0, 0};
     MPI_Dims_create(nprocs, 2, dims);
-    int nprow = dims[0];
-    int npcol = dims[1];
-    int context = 0;
-    char order = 'R';
+    int nprow = dims[0], npcol = dims[1], ctx = 0;
+    Cblacs_get(-1, 0, &ctx);
+    Cblacs_gridinit(&ctx, const_cast<char*>("R"), nprow, npcol);
 
-    Cblacs_get(-1, 0, &context);
-    Cblacs_gridinit(&context, &order, nprow, npcol);
+    double* globalH = GetGlobalH(rank, nprocs, ctx, a, w, N, Emax, Emin, sizes);
 
-    int myrow, mycol;
-    int nprows, npcols;
-    Cblacs_gridinfo(context, &nprows, &npcols, &myrow, &mycol);
+    if (mpiroot) {
+        std::vector<std::complex<double>> H(n * n);
+        for (int i = 0; i < n * n; ++i) {
+            H[i] = std::complex<double>(globalH[i], 0.0);
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                std::cout << H[i * n + j] << "\t";
+            }
+            std::cout << "\n";
+        }
+    }
 
-    int NBlockRo, MBlockRo, NBlockH, MBlockH;
-    int nrowsRo, ncolsRo, nrowsH, ncolsH;
-
-    // for (int i = 0; Emin <= Emax - i; i++) {
-    //     double* H_i = gen_H_i(ctxt, p, i);
-
-    //     write_H_i_in_H(ctxt, p, H, H_i, i);
-
-    //     delete[] H_i;
-    // }
-
-    Cblacs_gridexit(context);
+    Cblacs_gridexit(ctx);
     Cblacs_exit(0);
-
     return 0;
 }
--- task2/task2.h
+++ task2/task2.h
@@ -6,29 +6,30 @@ typedef std::complex<float> complex_s;
 typedef std::complex<double> complex_d;
 
 extern "C" {
-	void Cblacs_pinfo( int* mypnum, int* nprocs);
+	void Cblacs_pinfo(int* mypnum, int* nprocs);
 	void Cblacs_get(int context, int request, int* value);
-	int Cblacs_gridinit( int* context, char * order, int np_row, int np_col);
-	void Cblacs_gridinfo( int context, int*  np_row, int* np_col, int*  my_row, int*  my_col);
-	void Cblacs_gridexit( int context);
-	void Cblacs_exit( int error_code);
-	void Cblacs_gridmap( int* context, int* map, int ld_usermap, int np_row, int np_col);
+	int Cblacs_gridinit(int* context, char*  order, int np_row, int np_col);
+	void Cblacs_gridinfo(int context, int* np_row, int* np_col, int* my_row, int* my_col);
+	void Cblacs_gridexit(int context);
+	void Cblacs_barrier(int context, const char*);
+	void Cblacs_exit(int error_code);
+	void Cblacs_gridmap(int* context, int* map, int ld_usermap, int np_row, int np_col);
 
-	void Cblacs_pcoord(int, int, int *, int *);
-	void Cdgerv2d(int, int, int, double *, int, int, int);
-	void Cdgesd2d(int, int, int, double *, int, int, int);
+	void Cblacs_pcoord(int, int, int* , int* );
+	void Cdgerv2d(int, int, int, double* , int, int, int);
+	void Cdgesd2d(int, int, int, double* , int, int, int);
 
-	int npreroc_(int *n, int *nb, int *iproc, int *isrcproc, int *nprocs);
-	int numroc_(int *n, int *nb, int *iproc, int *isrcproc, int *nprocs);
+	int npreroc_(int* n, int* nb, int* iproc, int* isrcproc, int* nprocs);
+	int numroc_(int* n, int* nb, int* iproc, int* isrcproc, int* nprocs);
 
-	void descinit_(int *idescal, int *m, int *n, int *mb, int *nb, int *dummy1 , int *dummy2 , int *icon, int *procRows, int *info);
+	void descinit_(int* idescal, int* m, int* n, int* mb, int* nb, int* dummy1 , int* dummy2 , int* icon, int* procRows, int* info);
 
-	void psgesvd_(char *jobu, char *jobvt, int *m, int *n, float *a, int *ia, int *ja, int *desca, float *s, float *u, int *iu, int *ju, int *descu, float *vt, int *ivt, int *jvt, int *descvt, float *work, int *lwork, int *info);
-	void pdgesvd_(char *jobu, char *jobvt, int *m, int *n, double *a, int *ia, int *ja, int *desca, double *s, double *u, int *iu, int *ju, int *descu, double *vt, int *ivt, int *jvt, int *descvt, double *work, int *lwork, int *info);
-	void pcgesvd_(char *jobu, char *jobvt, int *m, int *n, complex_s *a, int *ia, int *ja, int *desca, float *s, complex_s *u, int *iu, int *ju, int *descu, complex_s *vt, int *ivt, int *jvt, int *descvt, complex_s *work, int *lwork, float *rwork, int *info);
-	void pzgesvd_(char *jobu, char *jobvt, int *m, int *n, complex_d *a, int *ia, int *ja, int *desca, double *s, complex_d *u, int *iu, int *ju, int *descu, complex_d *vt, int *ivt, int *jvt, int *descvt, complex_d *work, int *lwork, double *rwork, int *info);
+	void psgesvd_(char* jobu, char* jobvt, int* m, int* n, float* a, int* ia, int* ja, int* desca, float* s, float* u, int* iu, int* ju, int* descu, float* vt, int* ivt, int* jvt, int* descvt, float* work, int* lwork, int* info);
+	void pdgesvd_(char* jobu, char* jobvt, int* m, int* n, double* a, int* ia, int* ja, int* desca, double* s, double* u, int* iu, int* ju, int* descu, double* vt, int* ivt, int* jvt, int* descvt, double* work, int* lwork, int* info);
+	void pcgesvd_(char* jobu, char* jobvt, int* m, int* n, complex_s* a, int* ia, int* ja, int* desca, float* s, complex_s* u, int* iu, int* ju, int* descu, complex_s* vt, int* ivt, int* jvt, int* descvt, complex_s* work, int* lwork, float* rwork, int* info);
+	void pzgesvd_(char* jobu, char* jobvt, int* m, int* n, complex_d* a, int* ia, int* ja, int* desca, double* s, complex_d* u, int* iu, int* ju, int* descu, complex_d* vt, int* ivt, int* jvt, int* descvt, complex_d* work, int* lwork, double* rwork, int* info);
 
-    void pdgemm_(char*, char*, int*, int*, int*, double*, double*, int*, int*, int*, double*, int*, int*, int*, double*, double*, int*, int*, int*);
+	void pdgemm_(char*, char*, int*, int*, int*, double*, double*, int*, int*, int*, double*, int*, int*, int*, double*, double*, int*, int*, int*);
 }
 
 #endif
